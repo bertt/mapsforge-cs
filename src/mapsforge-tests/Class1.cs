ec6fc49d5d53c0d3fe9e71c2e1c8a4f75055a12a
bert <btemme@hotmail.com>
2018-03-18 10:06:16 +0100

    setting up

---
+++ src/mapsforge-tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace mapsforge_tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        public void FirstTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
---
+++ src/mapsforge/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mapsforge
+{
+    public class Class1
+    {
+    }
+}
